5bd813efee4a3e80fa1fb65fc303e5952768faa0
Sunken <114361572+sunkenball@users.noreply.github.com>
2023-01-30 16:04:39 +0000

    Update Main.lua

--- Promise/Main.lua
+++ Promise/Main.lua
@@ -1,43 +1,51 @@
 local Promise = {}
 
 function Promise.new(func: func): table | string
-    local Success, Output    = pcall(func)
-    if not Success then
-        return Promise, Output
-    else
-        return Promise
-    end
+    task.spawn(function()
+        local Success, Output    = pcall(func)
+        if not Success then
+            return Promise, Output
+        else
+            return Promise
+        end
+    end)
 end
 
 function Promise:andThen(func: func): table | string
-    local Success, Output    = pcall(func)
-    if not Success then
-        Promise:catch(warn, Output)
-        return Promise, Output
-    else
-        return Promise
-    end
+    task.spawn(function()
+        local Success, Output    = pcall(func)
+        if not Success then
+            Promise:catch(warn, Output)
+            return Promise, Output
+        else
+            return Promise
+        end
+    end)
 end
 
 function Promise:catch(func): table
-    return Promise
+    task.spawn(function()
+        return Promise
+    end)
 end
 
 function Promise.all(functions: table): boolean
-    local NotSuccessful = {}
-    for i,v in next, functions do
-        if type(v) == "function" then
-            local Success, Output = pcall(v)
-            if not Success then
-                local Line    =     Output:match(":(%d*):")
-                local Error   =     Output:split(":")[3]:sub(2, -1)
-                table.insert(NotSuccessful, string.format("Error At (%s): %s", tostring(Line), Error))
+    task.spawn(function()
+        local NotSuccessful = {}
+        for i,v in next, functions do
+            if type(v) == "function" then
+                local Success, Output = pcall(v)
+                if not Success then
+                    local Line    =     Output:match(":(%d*):")
+                    local Error   =     Output:split(":")[3]:sub(2, -1)
+                    table.insert(NotSuccessful, string.format("Error At (%s): %s", tostring(Line), Error))
+                end
             end
         end
-    end
-    local Successful   =    not (#NotSuccessful > 0)
-    local Errors       =    NotSuccessful
-    return Successful, Errors
+        local Successful   =    not (#NotSuccessful > 0)
+        local Errors       =    NotSuccessful
+        return Successful, Errors
+    end)
 end
 
 return Promise
